002e64ee9804312f8cee33a5e91fc1f04884adca
Nama Saya <86531458+monubot@users.noreply.github.com>
2021-08-11 22:19:52 +0700

    Update salam.py

--- userbot/modules/salam.py
+++ userbot/modules/salam.py
@@ -7,28 +7,88 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern='^.P(?: |$)(.*)')
+@register(outgoing=True, pattern="^.p(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb.`")
+    await typew.edit("**Assalamualaikum Dulu Dari Mon**")
 
 
-@register(outgoing=True, pattern='^.p(?: |$)(.*)')
+@register(outgoing=True, pattern=r"^\.pe(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Assalamu'alaikum wr. wb.`")
+    await typew.edit("**Punten, bismillah ada pap nyasar**")
 
 
-@register(outgoing=True, pattern='^.L(?: |$)(.*)')
+@register(outgoing=True, pattern="^.P(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Wa'alaikumssalam wr. wb.`")
+    await typew.edit("**Punten, bismillah nyantol cewe satu...**")
 
 
-@register(outgoing=True, pattern='^.l(?: |$)(.*)')
+@register(outgoing=True, pattern=r"^\.l(?: |$)(.*)")
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Wa'alaikumssalam wr. wb.`")
+    await typew.edit("**Wa'alaikumsalam dari mon**")
+
+
+@register(outgoing=True, pattern="^.m(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Punten, bismillah dapet sleepcall**")
+
+
+@register(outgoing=True, pattern="^.cw(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bismillah dapet admin cewe**")
+
+@register(outgoing=True, pattern="^.ba(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Punten, bismillah admin**")
+
+
+@register(outgoing=True, pattern="^.bad(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bismillah admin**")
+
+
+@register(outgoing=True, pattern="^.bd(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bismillah aja dulu**")
+
+
+@register(outgoing=True, pattern="^.bt(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Punten, bismillah dapet pap tangtop**")
+
+
+@register(outgoing=True, pattern="^.po(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bismillah dapet pap tangtop sekalian orangnya**")
+
+
+@register(outgoing=True, pattern="^.pt(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bismillah dapet pap tangtop**")
+
+
+@register(outgoing=True, pattern="^.bs(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bismillah dapet sleepcall**")
+
+
+@register(outgoing=True, pattern="^.tn(?: |$)(.*)")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("**Bismillah dapet pap tangtop nyasar**")
+
 
 
 CMD_HELP.update({
